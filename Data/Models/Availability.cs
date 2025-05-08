bf09f77b5e6d563bbf79caadf63b73595bda5728
NikitaShevchenko2 <nikiba.shevchenko@gmail.com>
2025-05-08 16:33:16 +0200

    Added class Availability

---
+++ Data/Models/Availability.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Availability
+    {
+        // class containing properties for availability
+        #region Properties
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        #endregion
+    }
+}
