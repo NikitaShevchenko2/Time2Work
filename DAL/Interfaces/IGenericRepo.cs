5e130ded287bc9e1fd741dcbbfa11d4c993bf87c
NikitaShevchenko2 <nikiba.shevchenko@gmail.com>
2025-05-19 19:06:44 +0200

    Added following classes: - IGenericRepo: DAL -IGenericService: SERVICES - BaseService: SERVICES

---
+++ DAL/Interfaces/IGenericRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IGenericRepo<T>
+    {
+        // interface containing CRUD for all DAL-repo's
+        // generic, class gets added in implementation
+        List<T> GetAll();
+        T GetById(int id);
+        void Update(T model);
+        void Add(T model);
+        void Delete(int id);
+    }
+}
---
+++ Services/Abstractions/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Abstractions
+{
+    public abstract class BaseService<T>
+    {
+        // shortcut for instatiating the repo, gets added in program.cs in MVC
+        protected IGenericRepo<T> _repo;
+
+        protected BaseService(IGenericRepo<T> repo)
+        {
+            _repo = repo;
+        }
+    }
+}
