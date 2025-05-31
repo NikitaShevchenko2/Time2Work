03ab2148ef5e0ebeacf88ec68e93584653885ff1
NikitaShevchenko2 <nikiba.shevchenko@gmail.com>
2025-05-31 10:42:18 +0200

    Added employee service

---
+++ Services/Implementations/EmployeeService.cs
@@ -0,0 +1,50 @@
+﻿using DAL.Interfaces;
+using Data.Models;
+using Services.Abstractions;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementations
+{
+    public class EmployeeService : BaseService<Employee>, IGenericService<Employee>
+    {
+        // class containing the services for the employee
+        #region Constructor
+        public EmployeeService(IGenericRepo<Employee> repo) : base(repo)
+        {
+            
+        }
+        #endregion
+
+        #region Methods
+        public void Add(Employee model)
+        {
+            _repo.Add(model);
+        }
+
+        public void Delete(int id)
+        {
+            _repo.Delete(id);
+        }
+
+        public List<Employee> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public Employee GetById(int id)
+        {
+            return _repo.GetById(id);
+        }
+
+        public void Update(Employee model)
+        {
+            _repo.Update(model);
+        }
+        #endregion
+    }
+}
